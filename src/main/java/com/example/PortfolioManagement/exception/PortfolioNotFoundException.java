a9b865831519f7d742bee20ac0e9c2f208b06071
mubashirp021 <pandithmubashir@gmail.com>
2026-08-03 15:00:32 +0000

    Remove old project structure

--- src/main/java/com/example/PortfolioManagement/exception/PortfolioNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.PortfolioManagement.exception;
-
-public class PortfolioNotFoundException extends RuntimeException {
-    public PortfolioNotFoundException(Long portfolioId) {
-        super("Portfolio not found with id: " + portfolioId);
-    }
-}
