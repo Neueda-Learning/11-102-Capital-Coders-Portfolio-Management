30db731ec60d5ef5fe75376d2d89686c960f1e73
mubashirp021 <pandithmubashir@gmail.com>
2026-08-03 14:28:45 +0000

    add PortfolioRepository

--- backend/src/main/java/com/portfolio/portfolio_management/model/Portfolio.java
+++ backend/src/main/java/com/portfolio/portfolio_management/model/Portfolio.java
@@ -2,5 +2,14 @@ package com.portfolio.portfolio_management.model;
 
 import java.time.LocalDate;
 
-public record Portfolio(int portfolioId, int empId, int fundId, String portfolioName, String portfolioDescription, String riskLevel, double allocatedAmount, LocalDate createdDate) {
+public record Portfolio(
+        Integer portfolioId,
+        Integer employeeId,
+        Integer fundId,
+        String portfolioName,
+        String portfolioDescription,
+        String riskLevel,
+        Double allocatedAmount,
+        LocalDate createdDate
+) {
 }
